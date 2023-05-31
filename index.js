3592dc7572d279b844dcf3a0d09c2763fde94a0a
mkaraki <26180919+mkaraki@users.noreply.github.com>
2023-05-31 14:09:47 +0900

    Update system

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
+const list = document.getElementById('list');
+const res_mtu = document.getElementById('res-mtu');
+const res_mss = document.getElementById('res-mss');
+
+const add = (name, size) => {
+    let div = document.createElement('div');
+    let close = document.createElement('span');
+    close.innerHTML = '&times;';
+    close.onclick = () => {
+        div.remove();
+        calc();
+    }
+    close.className = 'clickable';
+    div.innerText = `${name} (${size} bytes)`;
+    div.className = 'item';
+    div.dataset.size = size;
+    div.insertBefore(close, div.firstChild);
+    list.appendChild(div);
+    calc();
+};
+
 const calc = () => {
     let mtu = document.getElementById('n-ether-frame').value;
-    Array.prototype.forEach.call(document.getElementsByClassName('mtu'), (v) => {
-        if (!v.checked)
-            return;
-
+    Array.prototype.forEach.call(list.getElementsByClassName('item'), (v) => {
         if (v.dataset.size) {
             mtu -= v.dataset.size;
             return;
         }
-
-        if (v.dataset.sizefor) {
-            mtu -= document.getElementById(v.dataset.sizefor).value;
-            return;
-        }
     });
 
-    let mss = mtu;
-    Array.prototype.forEach.call(document.getElementsByClassName('mss'), (v) => {
-        if (!v.checked)
-            return;
-
-        if (v.dataset.size) {
-            mss -= v.dataset.size;
-            return;
-        }
-
-        if (v.dataset.sizefor) {
-            mss -= document.getElementById(v.dataset.sizefor).value;
-            return;
-        }
-    });
-
-    alert(`MTU: ${mtu}\nMSS: ${mss}`);
+    res_mtu.innerHTML = mtu;
+    res_mss.innerHTML = mtu - 40;
 };
